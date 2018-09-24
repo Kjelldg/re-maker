2d07bcc5c05b3e5cab5262ab82ada58b79a50077
kjelldg <Kjelldg@gmail.com>
2018-09-24 22:01:01 +0200

    Adding background image

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import "./index.css";
 import App from "./app/layout/App";
 import registerServiceWorker from "./registerServiceWorker";
 
+import Image from "/uDemy/Build an app with React, Redux/remake/src/app/layout/Backgrounds/mavica.jpg"; // Import using relative path
+
+const styles = {
+  paperContainer: {
+    backgroundImage: `url(${Image})`
+  }
+};
+
 const rootEl = document.getElementById("root");
 
 let render = () => {
@@ -17,5 +25,11 @@ if (module.hot) {
 }
 
 render();
+
+export default class Home extends React.Component {
+  render() {
+    return <div style={styles.paperContainer} />;
+  }
+}
 
 registerServiceWorker();
