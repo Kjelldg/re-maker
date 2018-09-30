618ab9ac1c5c5c031a8350562bd93005314ae19d
kjelldg <Kjelldg@gmail.com>
2018-09-30 22:02:17 +0200

    Added component EventDashboard

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Button } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
 
 class App extends Component {
   render() {
@@ -6,6 +8,12 @@ class App extends Component {
       <div className="App">
         <h1>Re-maker</h1>
         <p>A react app for makers.</p>
+        <button className="ui icon button">
+        <i className="smile icon"></i>
+        CSS Button
+        </button>
+        <Button icon="smile" content="React Button"/>
+        <EventDashboard/>
       </div>
     );
   }
