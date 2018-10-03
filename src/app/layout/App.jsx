d1db191fe1fd0efd4d378810ba7c3496ada80ea0
kjelldg <Kjelldg@gmail.com>
2018-10-03 20:02:20 +0200

    Button tests.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -4,18 +4,17 @@ import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <h1>Re-maker</h1>
         <p>A react app for makers.</p>
+        <button class="ui button">Semantic UI button test</button>
         <button className="ui icon button">
-        <i className="smile icon"></i>
-        CSS Button
+          <i className="smile icon" />
+          CSS Button
         </button>
-        <Button icon="smile" content="React Button"/>
-        <EventDashboard/>
-      </div>
-    );
+        <Button icon="smile" content="React Button" />
+        <EventDashboard />
+      </div>;
   }
 }
 export default App;
