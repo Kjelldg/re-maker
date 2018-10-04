1998fd2d0a580bca3f48ff6e5be74557740a1a2a
kjelldg <Kjelldg@gmail.com>
2018-10-04 20:33:56 +0200

    Testing React things.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import { Button } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import HeaderTest from "../../features/event/EventDashboard/HeaderTest"
 
 class App extends Component {
   render() {
     return <div className="App">
         <h1>Re-maker</h1>
         <p>A react app for makers.</p>
-        <button class="ui button">Semantic UI button test</button>
+        <HeaderTest />
+        <button className="ui button">Semantic UI button test</button>
         <button className="ui icon button">
           <i className="smile icon" />
           CSS Button
         </button>
-        <Button icon="smile" content="React Button" />
+        <Button icon="smile" content="React Button" />        
         <EventDashboard />
       </div>;
   }
---
+++ src/features/event/EventDashboard/HeaderTest.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class HeaderTest extends Component {
+  render() {
+    return <h1>Test text</h1>;
+  }
+}
+
+export default HeaderTest;
